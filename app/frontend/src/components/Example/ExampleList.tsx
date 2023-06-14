1742264807fa88897ef63f26796ad32b7019e94a
AlitalipSever <alitalipsever@gmail.com>
2023-06-14 14:26:41 +0200

    example list updated with related questions

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,12 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "Can you share any success stories or case studies of your digital transformation solutions in action? How did your company help a client achieve their goals through innovative technological advancements?",
+        value: "Can you share any success stories or case studies of your digital transformation solutions in action? How did your company help a client achieve their goals through innovative technological advancements?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "Do you have any references or testimonials from previous clients who have undergone digital transformation with your company? What were their specific challenges, and how did your solutions address and overcome those obstacles?", 
+    value: "Do you have any references or testimonials from previous clients who have undergone digital transformation with your company? What were their specific challenges, and how did your solutions address and overcome those obstacles?" },
+    { text: "I'm interested in learning about real-world examples of how your digital transformation services have made a significant impact on businesses. Could you provide a specific case study where your company's expertise revolutionized a client's operations, leading to increased efficiency, cost savings, or improved customer experience?", value: "I'm interested in learning about real-world examples of how your digital transformation services have made a significant impact on businesses. Could you provide a specific case study where your company's expertise revolutionized a client's operations, leading to increased efficiency, cost savings, or improved customer experience?" }
 ];
 
 interface Props {
